c1711dff2e546594bd0b1ac9c4164d0a98dcf099
mullachv <mullachv@gmail.com>
2015-10-25 17:00:06 -0400

    command line options

--- src/p5_GravitationalVoronoiGame/Main.java
+++ src/p5_GravitationalVoronoiGame/Main.java
@@ -12,14 +12,43 @@ public class Main {
 	final String host = "localhost";
 	final int port = 1337;
 	
+	//$java -jar client.jar -s3 myteamname
 	public static void main(String[] args) throws Exception{
 		Main m = new Main();
 		m.tcpClient = new TCPClient();
 		m.tcpClient.startTCP(m.host, m.port);
 		m.board = new Board();
+
+		if (args.length > 0 ) {
+			if (args[0].substring(0, 2).equalsIgnoreCase("-s")) {
+				String ar = args[0].substring(2, 3);
+				if (ar.equalsIgnoreCase("R")) {
+					m.strategy = new RandomStrategy();					
+				} else if (ar.equalsIgnoreCase("O")) {
+					m.strategy = new OnePixelOff();
+				} else if (ar.equalsIgnoreCase("1")) {
+					m.strategy = new OnePixelToMaxScoreStrategy();
+				} else if (ar.equalsIgnoreCase("2")) {
+					m.strategy = new OnePixelToMaxScoreStrategyV2();
+				} else if (ar.equalsIgnoreCase("3")) {
+					m.strategy = new OnePixelToMaxScoreStrategyV3();
+				} else if (ar.equalsIgnoreCase("4")) {
+					m.strategy = new OnePixelToMaxScoreStrategyV4();
+				} else if (ar.equalsIgnoreCase("M")) {
+					m.strategy = new MinMaxScoreStrategy();
+				} else if (ar.equalsIgnoreCase("X")) {
+					m.strategy = new RandomMaxPullLocationStrategy();
+				} else if (ar.equalsIgnoreCase("L")) {
+					m.strategy = new RandomMaxScoreLocationStrategy();
+				}
+			}
+			if (args.length > 1) {
+				m.self = args[1];
+			}
+		}
 		
 		// change to your strategy implementation here
-		m.strategy = new OnePixelToMaxScoreStrategyV3();
+		//m.strategy = new OnePixelToMaxScoreStrategyV3();
 		
 		m.run();
 		
